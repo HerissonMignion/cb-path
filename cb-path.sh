4bda2c13a14bbcf4794acc67ce8127f4f8cd17b1
Charles Benca <zzazzbug@gmail.com>
2024-12-28 16:18:22 -0500

    tout réimplémenté en mieux

--- cb-path.sh
+++ cb-path.sh
@@ -1,10 +1,18 @@
-#!/bin/bash
 
 
-# this file is meant to be source(ed) into your shell
 
-CB_PATH_PATH_BACKUP="${PATH}";
+# this file is meant to be sourced by bash.
 
+
+# CB_PATH_PATH_BACKUP="";
+
+
+# filter les caractères / . dans les noms de fichiers. le . c'est pour
+# *facilement* pouvoir utiliser * (filename expansion) pour lister
+# tout les fichiers dans le dossier save.
+--cb-path--filter-name() {
+	echo "${1//[\/.]/}";
+}
 
 
 # TODO:
@@ -13,192 +21,194 @@ CB_PATH_PATH_BACKUP="${PATH}";
 # cd : changer le current directory pour le path associé
 
 cb-path () {
+	if [ -z "$CB_PATH_PATH_BACKUP" ]; then
+		CB_PATH_PATH_BACKUP="${PATH}";
+	fi;
 	
-	IFS=$'\n';
-	local db_file_path="$HOME/.cb-path-db";
-	touch "$db_file_path";
+	local config_dir=~/.local/share/cb-path;
+	local save_dir="$config_dir/saved";
+	local namepath;
+	mkdir -p "$config_dir";
+	mkdir -p "$save_dir";
 	
-	cb-path-help () {
-		(echo $'Manage the PATH environment variable';
-		echo $'';
-		echo $'db file path : '"$db_file_path";
-		echo $'';
-		echo $'usage: cb-path [command] [options]';
-		echo $'commands:';
-		echo $'\tlist : list your saved folders with their names';
-		echo $'\treset : reset your PATH to its value when opening your shell';
-		echo $'\texport <name> : export the associated folder(s) to your PATH';
-		echo $'\tadd <name> : add the current directory to the database with the associated name';
-		echo $'\trm <name> : remove the name from the database';
-		echo $'\tcd <name> : change the current directory to the associated path';
-		echo $'\tls <name> : list files in the associated folder(s)';
-		echo $'\twhich <file names> : find where a file is located and to which name its parent directory is associated';
-		echo $'';) | fmt -s;
-	}
+	# IFS=$'\n';
+	# local db_file_path="$HOME/.cb-path-db";
+	# touch "$db_file_path";
+    
 	
-	cb-path-read-db () {
-		
-		cat "$db_file_path";
-		
-	}
 	
 	if [ "$#" == "0" ]; then
-		echo "not enough arguments";
+		echo "Not enough arguments";
 		return 1;
 	fi
 	
 	
+
+	local option_help=0;
 	
-	
-	for i in $*; do
-		if [ "$i" == "-h" ] || [ "$i" == "--help" ] || [ "$i" == "help" ]; then
-			cb-path-help;
-			return;
-		fi
+	for i in "$@"; do
+		case "$i" in
+			(-h|--help|help)
+				option_help=1;
+				;;
+			(--)
+				break;
+				;;
+		esac;
 	done
 	
 	
-	
+	if (($option_help)); then
+		cat <<"HELP";
+SYNOPSIS
+
+	cb-path [-h|--help] <command> [<arguments>]...
+
+DESCRIPTION
+
+	Manage the PATH environment variable.
+
+COMMANDS
+
+	list
+
+		List your saved folders with their names.
+
+	reset
+
+		Reset your PATH to its value when opening your shell.
+
+	export <name>
+
+		Export the associated folder to your PATH.
+
+	add <name>
+	add <name> <relative/absolute path>
+
+		Add the current directory to the database with the associated
+		name.
+
+	rm <name>
+
+		Remove the name from the database.
+
+	cd <name>
+
+		Change the current directory to the associated path.
+
+	ls <name> [<options>]...
+
+		List files in the associated folder. It uses the ls command on
+		the system, and forwards the arguments to it.
+
+	which <file names>
+
+		Find where a file is located and to which name its parent
+		directory is associated.
+
+HELP
+		return;
+	fi;
 	
 	
 	if [ "$1" == "list" ]; then
-		local content="$(cb-path-read-db)";
-		echo $';name\tpath';
-		for line in $content; do
-			local name="$(echo "$line" | cut "-d/" "-f1")";
-			local path="$(echo "$line" | cut "-d/" "-f2-")";
-			echo "$name"$'\t'"$path";
-		done
-		
+		for namepath in "$save_dir"/*; do
+			local name=$(basename "$namepath");
+			echo "$name:$(cat "$namepath")";
+		done;
 		
 	elif [ "$1" == "export" ]; then
-		if [ "$#" == "1" ]; then
-			echo "not enough arguments";
+		shift;
+		if ! [ "$#" == 1 ]; then
+			echo "Not enough or too many arguments." >&2;
 			return 1;
-		elif [ "$#" != "2" ]; then
-			echo "too many arguments";
-			return 1;
-		fi
-		
-		local content="$(cb-path-read-db)";
-		for line in $content; do
-			local name="$(echo "$line" | cut "-d/" "-f1")";
-			local path="$(echo "$line" | cut "-d/" "-f2-")";
-			
-			if [ "$name" == "$2" ]; then
-				export PATH="${path}:${PATH}";
-				echo "appended \"${path}\"";
-			fi
-		done
-		
+		fi;
+		local name=$(basename "$1");
+		if [ -f "$save_dir/$name" ] && [ -r "$save_dir/$name" ]; then
+			export PATH=$(cat "$save_dir/$name")":$PATH";
+		else
+			echo "Name \"$name\" does not exist." >&2;
+		fi;
 		
 	elif [ "$1" == "add" ]; then
-		if [ "$#" == "1" ]; then
-			echo "not enough arguments";
+		shift;
+		if ! [ "$#" == 1 ] && ! [ "$#" == 2 ]; then
+			echo "Not enough or too many arguments." >&2;
 			return 1;
-		elif [ "$#" != "2" ]; then
-			echo "too many arguments";
+		fi;
+		local name=$(--cb-path--filter-name "$1");
+	    if [ -f "$save_dir/$name" ]; then
+			echo "\"$name\" is already taken." >&2;
 			return 1;
-		fi
-		
-		echo "$2/$(pwd)" >> $db_file_path;
+		fi;
+		if ! [ -z "$2" ]; then
+			realpath -m "$2";
+		else
+			pwd;
+		fi > "$save_dir/$name";
 		
 	elif [ "$1" == "rm" ]; then
-		if [ "$#" == "1" ]; then
-			echo "not enough arguments";
+		shift;
+		if ! [ "$#" == 1 ]; then
+			echo "Not enough or too many arguments." >&2;
 			return 1;
-		elif [ "$#" != "2" ]; then
-			echo "too many arguments";
+		fi;
+		local name=$(--cb-path--filter-name "$1");
+		if [ -f "$save_dir/$name" ]; then
+			rm -r "$save_dir/$name";
+		else
+			echo "name \"$name\" is not used." >&2;
 			return 1;
-		fi
-		
-		local content="$(cb-path-read-db)";
-		local newcontent="";
-		local first="true";
-		for line in $content; do
-			local name="$(echo "$line" | cut "-d/" "-f1")";
-			local path="$(echo "$line" | cut "-d/" "-f2-")";
-			
-			if [ "$name" != "$2" ]; then
-				# echo "$line";
-				if [ "$first" == "false" ]; then
-					newcontent="$newcontent"$'\n';
-				fi
-				newcontent="${newcontent}${line}";
-				first="false";
-			else
-				echo "removed \"${path}\"";
-			fi
-		done
-		# echo -n "$newcontent";
-		echo -n "$newcontent" > $db_file_path;
-		
+		fi;
+
 	elif [ "$1" == "reset" ]; then
 		export PATH="$CB_PATH_PATH_BACKUP";
 		
 	elif [ "$1" == "cd" ]; then
-		if [ "$#" == "1" ]; then
-			echo "not enough arguments";
+		shift;
+		if ! [ "$#" == 1 ]; then
+			echo "Not enough or too many arguments." >&2;
 			return 1;
-		elif [ "$#" != "2" ]; then
-			echo "too many arguments";
+		fi;
+		local name=$(--cb-path--filter-name "$1");
+		if ! [ -f "$save_dir/$name" ]; then
+			echo "Name \"$name\" does not exist." >&2;
 			return 1;
-		fi
+		fi;
+	    cd "$(cat "$save_dir/$name")";
 		
-		local content="$(cb-path-read-db)";
-		for line in $content; do
-			local name="$(echo "$line" | cut "-d/" "-f1")";
-			local path="$(echo "$line" | cut "-d/" "-f2-")";
-			
-			if [ "$name" == "$2" ]; then
-				echo "cd ${path}";
-				cd "${path}";
-			fi
-		done
 	elif [ "$1" == "ls" ]; then
-		if [ "$#" == "1" ]; then
-			echo "not enough arguments";
+		shift;
+		if (("$#" < 1)); then
+			echo "Not enough arguments." >&2;
 			return 1;
-		elif [ "$#" != "2" ]; then
-			echo "too many arguments";
+		fi;
+		local name=$(--cb-path--filter-name "$1");
+		if ! [ -f "$save_dir/$name" ]; then
+			echo "Name \"$name\" does not exist." >&2;
 			return 1;
-		fi
-		
-		local content="$(cb-path-read-db)";
-		for line in $content; do
-			local name="$(echo "$line" | cut "-d/" "-f1")";
-			local path="$(echo "$line" | cut "-d/" "-f2-")";
-			
-			if [ "$name" == "$2" ]; then
-				ls -p "${path}" | grep -v "/";
-			fi
-		done
+		fi;
+		shift;
+		ls "$@" "$(cat "$save_dir/$name")";
 		
 	elif [ "$1" == "which" ]; then
-		if [ "$#" == "1" ]; then
-			echo "not enough arguments";
-			return 1;
-		fi
-		
 		shift;
-		local content="$(cb-path-read-db)";
-		for filename in $*; do
-			for line in $content; do
-				local name="$(echo "$line" | cut "-d/" "-f1")";
-				local path="$(echo "$line" | cut "-d/" "-f2-")";
-				
-				if [ -f "${path}/${filename}" ]; then
-					echo "$name"$'\t'"${path}/${filename}";
-				fi
-				
-			done
-		done
+		if ! [ "$#" == 1 ]; then
+			echo "Not enough or too many arguments." >&2;
+			return 1;
+		fi;
+		for namepath in "$save_dir"/*; do
+			local name=$(basename "$namepath");
+		    local refpath=$(cat "$namepath");
+			if [ -f "$refpath/$1" ] && [ -x "$refpath/$2" ]; then
+				echo "$name:$refpath";
+			fi;
+		done;
 		
 	else
-		echo "unknown command \"$1\"";
+		echo "unknown command \"$1\"" >&2;
 		return 1;
-	fi
-	
+	fi;
 }
 
 
